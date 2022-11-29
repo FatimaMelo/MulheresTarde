5dd625c7c56f014a19cc69e40de9dbfe614c6336
valquiriafrz <vallmachado0047@gmail.com>
2022-11-29 16:46:30 -0300

    atualização

---
+++ aula/Roupas.php
@@ -0,0 +1,30 @@
+<?php
+    require_once 'head.php';
+    require_once 'menu.php';
+    ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8 text-center">
+                <h2>Roupas</h2>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid imagens">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="imagens/lookacademia1.jpg" class="img-fluid">
+            </div>
+
+            <div class="col-md-4">
+                <img src="imagens/lookacademia2.jpg" class="img-fluid">
+            </div>
+
+            <div class="col-md-4">
+                <img src="imagens/lookacademia3.jpg" class="img-fluid">
+            </div>
+
+        </div>
+    </div>
+    <?php
+    require_once 'footer.php'
+    ?>
