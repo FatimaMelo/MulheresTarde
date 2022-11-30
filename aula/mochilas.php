e3f9fdbb14c5bbe05d869e509c9f2b50f6d017cc
FatimaMelo <93003149+FatimaMelo@users.noreply.github.com>
2022-11-30 13:19:37 -0300

    Add files via upload

---
+++ aula/mochilas.php
@@ -0,0 +1,71 @@
+<?php
+  require_once 'head.php';
+  require_once 'menu.php';
+?>  
+
+    <div class="container-fluid conteudo">
+        <div class="row">
+            <div class="col-md-12 text-center">
+               <h1>Coleção 2023</h1>
+            </div>
+
+        </div>
+    </div>
+
+   
+    <div class="container-fluid imagens">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="imagens/mochila1.jpg">
+                    <div class="card-body">
+                        <h3 class="card-text">Mochila Paloma</h3>
+                        <h4>R$ 150,00</h4>
+                    </div>
+                </div> 
+            </div>
+
+            <div class="col-md-3">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="imagens/mochila2.jpg">
+                    <div class="card-body">
+                    <h3 class="card-text">Mochila Raissa</h3>
+                        <h4>R$ 120,00</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="imagens/mochila3.jpg">
+                    <div class="card-body">
+                    <h3 class="card-text">Mochila Mayra</h3>
+                        <h4>R$ 110,00</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="imagens/mochila4.jpg">
+                    <div class="card-body">
+                    <h3 class="card-text">Mochila Academia</h3>
+                        <h4>R$ 80,00</h4>
+                    </div>
+                </div>
+
+        </div>
+    </div>
+
+   
+    <?php
+        require_once 'footer.php';
+
+    ?>
+ 
+
+
+
+
+
+   
