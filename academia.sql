828dab436b74514a1fc747309dbc4f615c32c583
FatimaMelo <93003149+FatimaMelo@users.noreply.github.com>
2022-12-08 16:12:40 -0300

    Add files via upload

--- academia.sql
+++ academia.sql
@@ -64,6 +64,31 @@ create table aula(
 	foreign key(idprofessor)references professor(idprofessor),
 	foreign key(idatividade)references atividade(idatividade));
 
+create table aulaaluno(
+	idaulaaluno integer primary key auto_increment,
+	matricula integer not null,
+	idaula integer not null,
+	foreign key(matricula)references aluno(matricula),
+	foreign key(idaula)references aula(idaula));
+	
+create table produto(
+	codigoproduto integer primary key auto_increment,
+	nome varchar(60)not null,
+	cor varchar(30)not null,
+	valor double not null,
+	tamanho char(2)not null,
+	quantidade integer not null);
+	
+create table venda(
+	idvenda integer primary key auto_increment,
+	data date not null,
+	valor double not null,
+	quantidade integer not null,
+	codigoproduto integer not null,
+	cpffuncionario char(14)not null,
+	foreign key(codigoproduto)references produto(codigoproduto),
+	foreign key(cpffuncionario)references funcionario(cpffuncionario));
+	
 
 	
 
