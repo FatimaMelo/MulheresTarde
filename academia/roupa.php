b5cf926ac0ab64d75d6890dea3789c106da7a11c
FatimaMelo <93003149+FatimaMelo@users.noreply.github.com>
2022-12-15 15:32:01 -0300

    Add files via upload

---
+++ academia/roupa.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once 'head.php';
+    require_once 'menu.php';
+    ?>
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md">
+                <h1>Nova Coleção</h1>
+            </div>
+        </div>
+    </div>
+
+    <?php
+        require_once 'footer.php';
+    ?>
+
+
+
+
+
+
+<
