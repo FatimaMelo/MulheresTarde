ef0658348c009512fe161bca81d4c9806e335820
FatimaMelo <93003149+FatimaMelo@users.noreply.github.com>
2022-12-20 19:50:12 -0300

    Add files via upload

--- aula/frmaluno.php
+++ aula/frmaluno.php
@@ -5,66 +5,130 @@
 <form>
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
+                <div class="col-md-12 text-center">
+                    <h3>Cadastro de Aluno</h3>
+                </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-5">
                 <div class="form-group">
                     <label for="nome">Nome</label>
-                    <input type="text" class="form-control" placeholder="Seu nome">    
+                    <input type="text" class="form-control" name="nome">    
                 </div>
             </div>           
 
-            <div class="col-md-4">
+            <div class="col-md-2">
                 <div class="form-group">
                     <label for="telefone">Telefone</label>
                     <input type="text" class="form-control" onkeypress="$(this).mask('(00)00000-0000')">
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-md-3">
               <div class="form-group">
-                 <label for="sexo">Sexo</label><p>
-                  <input type="radio" name="optradio" checked> Feminino           
+                 <label for="sexo">Sexo</label>
+                 <p><input type="radio" name="optradio" checked> Feminino           
                   <input type="radio" name="optradio"> Masculino
                 </div>
             </div>
-        </div>
-        
-        <div class="row">
-            <div class="col-md-4">        
+
+            <div class="col-md-2">        
               <div class="form-group">            
                   <label for="dn">Data de Nascimento</label>
                   <input type="date" class="form-control">
               </div>
            </div>
-
-            <div class="col-md-4">        
+        </div>
+        
+        <div class="row">
+           
+            <div class="col-md-3">        
                 <div class="form-group">            
                     <label for="cpf">Cpf</label>
                     <input type="text" class="form-control" onkeypress="$(this).mask('000.000.000-00');">
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="form-group">
-                <label for="rg">RG</label>
-                <input type="text" class="form-control">
-              </div>
+                    <label for="rg">RG</label>
+                    <input type="text" class="form-control">
+                </div>
             </div>
 
-  <div class="form-group">
-    <label for="exampleInputEmail1">Endereço de email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
-    <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Senha</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Clique em mim</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Enviar</button>
-  </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Endereço de email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">                   
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="row">
+            
+            <div class="col-md-2">            
+                <div class="form-group">
+                    <label for="cep">Cep</label>
+                    <input type="text" name="cep" class="form-control" id="cep" onblur="pesquisacep(this.value);">                    
+                </div>
+            </div>
+
+            <div class="col-md-6">            
+                <div class="form-group">
+                    <label for="endereco">Endereço</label>
+                    <input type="text" class="form-control" id="rua" name="rua">
+                </div>
+            </div>
+
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label for="numero">Número</label>
+                    <input type="text" class="form-control">    
+                </div>
+            </div> 
+            
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label for="complemento">Complemento</label>
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+        </div>
+
+        <div class="row">     
+
+            <div class="col-md-5">
+              <div class="form-group">
+                 <label for="bairro">Bairro</label><p>
+                 <input type="text" class="form-control" id="bairro" name="bairro">
+                </div>
+            </div>
+
+            <div class="col-md-5">
+              <div class="form-group">
+                 <label for="cidade">Cidade</label><p>
+                 <input type="text" class="form-control" id="cidade" name="cidade">
+                </div>
+            </div>
+
+            <div class="col-md-2  ">
+              <div class="form-group">
+                 <label for="uf">Estado</label><p>
+                 <input type="text" class="form-control" id="uf" name="uf">
+                </div>
+            </div>
+        </div>
+        
+        <div class="row">    
+            <div class="col-md-12 text-right">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+               
+            </div>
+        </div>
+    </div>
   
 </form>
 
