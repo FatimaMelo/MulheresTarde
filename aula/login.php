1c6fcec93f6d61029bbd423200cda06ec7a46771
FatimaMelo <93003149+FatimaMelo@users.noreply.github.com>
2023-01-16 14:16:51 -0300

    Add files via upload

--- aula/login.php
+++ aula/login.php
@@ -1,23 +1,56 @@
 <?php
         include_once 'conexao.php';
+
+        session_start();
+	    ob_start();
 ?>
 
 <?php
+
+//echo "senha".password_hash(123, PASSWORD_DEFAULT);
 
 $dadoslogin = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
 if (!empty($dadoslogin['btnlogin'])) {
 
-$buscalogin = "SELECT matricula, nome, email,senha
-                        FROM aluno 
-                        WHERE email =:usuario  
-                        LIMIT 1";
+        $buscalogin = "SELECT matricula, nome, email,senha
+                                FROM aluno 
+                                WHERE email =:usuario  
+                                LIMIT 1";
 
-$resultado= $conn->prepare($buscalogin);
-$resultado->bindParam(':usuario', $dadoslogin['usuario'], PDO::PARAM_STR);
-$resultado->execute();
+        $resultado= $conn->prepare($buscalogin);
+        $resultado->bindParam(':usuario', $dadoslogin['usuario'], PDO::PARAM_STR);
+        $resultado->execute();
 
+        if(($resultado) AND ($resultado->rowCount() != 0)){
+            $resposta = $resultado->fetch(PDO::FETCH_ASSOC);
+            //var_dump($resposta);
+            //var_dump($dadoslogin);
+
+            if(password_verify($dadoslogin['senha'], $resposta['senha'])){
+                
+                $_SESSION['nome'] = $resposta['nome'];
+                header("Location: administrativo.php");
+
+            }
+            else{
+                $_SESSION['msg'] = "Erro: Usuário ou senha inválida!";
+          
+            }
+
+        }
+        else{
+            $_SESSION['msg'] = "Erro: Usuário ou senha inválida!";
+        
+        }
 }
+
+
+if(isset($_SESSION['msg'])){
+    echo $_SESSION['msg'];
+    unset($_SESSION['msg']);
+}
+
 
 
 
