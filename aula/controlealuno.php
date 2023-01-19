7d3082fe30f354a815b740f6f9bcfc75bb107522
FatimaMelo <93003149+FatimaMelo@users.noreply.github.com>
2023-01-19 14:53:42 -0300

    Add files via upload

--- aula/controlealuno.php
+++ aula/controlealuno.php
@@ -5,8 +5,7 @@
     if(isset($_SESSION['msg'])){
         echo $_SESSION['msg'];
         unset($_SESSION['msg']);
-    }
-    
+    }    
 
 
     try{
@@ -82,6 +81,63 @@ if (!$vazio) {
     }
 
 }
+
+}
+
+
+if (!empty($dadoscad['btneditar'])) {
+
+    $dadoscad = array_map('trim', $dadoscad);
+
+    if(!filter_var($dadoscad['email'], FILTER_VALIDATE_EMAIL)) {
+        $vazio = true;
+
+        echo "<script>
+        alert('Informe um e-mail válido!!');
+        parent.location = 'frmaluno.php';
+        </script>";
+        
+        
+    }
+
+    $sql = "UPDATE aluno set nome=:nome,sexo=:sexo,datanascimento=:datanascimento,
+    telefone=:telefone,cpf=:cpf,rg=:rg,cep=:cep,numerocasa=:numerocasa,
+    complemento=:complemento,foto=:foto,email=:email WHERE matricula =:matricula";
+
+    $salvar= $conn->prepare($sql);
+    $salvar->bindParam(':nome', $dadoscad['nome'], PDO::PARAM_STR);
+    $salvar->bindParam(':sexo', $dadoscad['sexo'], PDO::PARAM_STR);
+    $salvar->bindParam(':datanascimento', $dadoscad['dn'], PDO::PARAM_STR);
+    $salvar->bindParam(':telefone', $dadoscad['telefone'], PDO::PARAM_STR);
+    $salvar->bindParam(':cpf', $dadoscad['cpf'], PDO::PARAM_STR);
+    $salvar->bindParam(':rg', $dadoscad['rg'], PDO::PARAM_STR);
+    $salvar->bindParam(':cep', $dadoscad['cep'], PDO::PARAM_STR);
+    $salvar->bindParam(':numerocasa', $dadoscad['numero'], PDO::PARAM_INT);
+    $salvar->bindParam(':complemento', $dadoscad['complemento'], PDO::PARAM_STR);
+    $salvar->bindParam(':foto', $dadoscad['foto'], PDO::PARAM_STR);
+    $salvar->bindParam(':email', $dadoscad['email'], PDO::PARAM_STR);
+    $salvar->bindParam(':matricula', $dadoscad['matricula'], PDO::PARAM_INT);
+    $salvar->execute();
+
+    if ($salvar->rowCount()) {
+        
+        echo "<script>
+        alert('Dados Atualizadoscom sucesso!!');
+        parent.location = 'relalunos.php';
+        </script>";
+
+        unset($dadoscad);
+    } else {
+        echo "<script>
+        alert('Aluno não cadastrado!');
+        parent.location = 'relalunos.php';
+        </script>";
+        
+    }
+
+
+
+
 
 }
 
