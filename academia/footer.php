fb01c54c704a3f901f151ebc553bea88c9c7d4ae
FatimaMelo <93003149+FatimaMelo@users.noreply.github.com>
2023-01-26 16:50:04 -0300

    Add files via upload

---
+++ academia/footer.php
@@ -0,0 +1,50 @@
+<footer class="">
+<div class="container-fluid">
+      <div class="row text-center bg-light">
+        <div class="col-md-3">
+          <h4>Institucional</h4>
+          <p><a href="#">Home</a></p>
+          <p><a href="#">Nossa Academia</a></p>
+          <p><a href="#">Conheça os Planos</a></p>
+          <p><a href="#">Área do Cliente</a></p>
+        </div>
+
+        <div class="col-md-3">
+          <h4>Atividades</h4>
+          <ul class="navbar-nav mr-auto">
+            <li class="navbar-item"><a class="nav-link" href="#">Spinning</a></li>
+            <li class="navbar-item"><a class="nav-link" href="#">Jumpp</a></li>
+            <li class="navbar-item"><a class="nav-link" href="#">Funcional</a></li>
+            <li class="navbar-item"><a class="nav-link" href="#">Pilates</a></li>
+          </ul>
+        </div>
+
+        <div class="col-md-3">
+          <h4>Funcionamento</h4>
+          <p>Segunda à Sábado - 06:00 às 23:00</p>
+          <p>Domingo - 07:00 às 13:00</p>
+        </div>  
+        
+        <div class="col-md-3 mapa">
+          <h4>Localização</h4>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.356219020248!2d-43.56103518503453!3d-22.900228385015083!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9be15839e68c4f%3A0x588a284ae162bc38!2sSenac%20Campo%20Grande!5e0!3m2!1spt-PT!2sbr!4v1669141477166!5m2!1spt-PT!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+
+      <div class="row text-center">
+        <div class="col-md-12 sociais">
+          <a href="#"><img src="imagens/whatsapp.png"></a>
+          <a href="#"><img src="imagens/face.png"></a>
+          <a href="#"><img src="imagens/insta.png"></a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+     <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ academia/index.php
@@ -0,0 +1,92 @@
+<?php
+  require_once 'head.php';
+  require_once 'menu.php';
+
+?>  
+
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <img src="imagens/academia3.jpg" class="img-fluid">
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container-fluid texto">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <h2>Conheça Algumas de Nossas Atividades</h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid imagens">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="imagens/academia1.jpg" class="img-fluid">
+            </div>
+
+            <div class="col-md-4">
+                <img src="imagens/academia2.jpg" class="img-fluid">
+            </div>
+
+            <div class="col-md-4">
+                <img src="imagens/academia4.webp" class="img-fluid">
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container-fluid text-center conteudo">
+      <div class="row">
+        <div class="col-md-6 col-sm">
+          <img src="imagens/jumpp.jpg" class="img-fluid">
+        </div>
+        <div class="col-md-6 col-sm">
+          <h3>Jumpp</h3>
+            <p>Muitas pessoas procuram o Jump para fortalecer as pernas e os glúteos, mas, mais do que essas regiões, ele trabalha muito bem a região do abdômen, e muitos dos exercícios são localizados para cuidar também dos músculos das costas e dos braços.</p>
+            <p>Por ser um exercício aeróbico intenso, uma aula de jump completa pode promover gasto calórico de até 900 calorias em 60 minutos. Essa elevada queima calórica pode ajudar na perda de peso, especialmente se o praticante combinar com uma dieta balanceada e realizar os exercícios com regularidade.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid text-center conteudo">
+      <div class="row">
+        <div class="col-md-6 col-sm">
+          <h3>Hidroginástica</h3>
+          <p>A hidroginástica melhora a força muscular, resistência, flexibilidade e equilíbrio. Comparada a exercícios terrestres, reduz o impacto sobre articulações e apresenta menor risco de lesões. Bom, né? Por tudo isso, a prática é recomendada para melhorar a capacidade motora funcional e independência da população idosa.</p>
+          <p>Mas, os benefícios podem alcançar ainda mais pessoas. Afinal, manter o corpo forte e saudável é bom em qualquer idade. Concorda? A hidroginástica pode ser uma ótima opção para quem quer sair do sedentarismo, para gestantes ou, simplesmente, para quem derrete de calor só de pensar em exercícios físicos no verão. </p>
+        </div>
+        <div class="col-md-6 col-sm">
+          <img src="imagens/hidro.jpg" class="img-fluid">
+          
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid text-center conteudo">
+      <div class="row">
+        <div class="col-md-6 col-sm">
+          <img src="imagens/spinning.jpg" class="img-fluid">
+        </div>
+        <div class="col-md-6 col-sm">
+          <h3>Spinning</h3>
+          <p>Os principais benefícios do spinning para quem pratica são: trabalhar todo o condicionamento físico e muscular, principalmente das pernas e glúteos; ajudar a tonificar as veias e melhorar os movimentos, o que contribui para a redução da pressão arterial, diminuindo os riscos de doenças cardíacas</p>
+        </div>
+      </div>
+    </div>
+
+    <?php
+        require_once 'footer.php';
+
+    ?>
+ 
+
+
+
+
+
+   
